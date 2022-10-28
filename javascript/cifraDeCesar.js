86d1fd3bf50de9f3281c582362a991580bcf6e10
elguesabal <113626409+elguesabal@users.noreply.github.com>
2022-10-28 15:29:41 -0300

    Add files via upload

---
+++ javascript/cifraDeCesar.js
@@ -0,0 +1,262 @@
+// POSICAO DAS LETRAS
+
+// a    b    c    d    e    f    g    h    i    j    k    l    m    n    o    p    q    r    s    t    u    v    w    x    y    z
+// 97   98   99   100  101  102  103  104  105  106  107  108  109  110  111  112  113  114  115  116  117  118  119  120  121  122
+
+// A    B    C    D    E    F    G    H    I    J    K    L    M    N    O    P    Q    R    S    T    U    V    W    X    Y    Z
+// 65   66   67   68   69   70   71   72   73   74   75   76   77   78   79   80   81   82   83   84   85   86   87   88   89   90
+
+                                                    // ESPACO 32
+
+
+
+
+
+
+
+
+
+
+
+
+// https://www.youtube.com/watch?v=utiLWBXmNQU
+// https://www.youtube.com/watch?v=UShV_TFxs_A
+// https://www.youtube.com/watch?v=dPl3OTOOJwM
+// https://stackoverflow.com/questions/5964373/is-there-a-difference-between-s-g-and-s-g
+
+
+
+
+
+
+                    //MEUS FAILLS
+
+                        //1
+// if (msg = 'a') {
+//     msg.innerText = 'aki entra o switch'
+//   }
+  
+// function criptografar() {
+//     switch (msg) {
+//     case a: 
+//         msg.innerText += 'b'
+//     // SEM break pra continuar rodando
+    
+//     case b:
+//         msg.innerText += 'c'
+    
+//     case c:
+//         msg.innerText += 'd'
+    
+//     case d:
+//         msg.innerText += 'e'
+    
+//     case e:
+//         msg.innerText += 'f'
+    
+//     case f:
+//         msg.innerText += 'g'
+    
+//     case g:
+//         msg.innerText += 'h'
+    
+//     case h:
+//         msg.innerText += 'i'
+    
+//     case i:
+//         msg.innerText += 'j'
+    
+//     case j:
+//         msg.innerText += 'k'
+//   }
+//   document.write(msg)
+// }
+
+
+
+                        //2
+// var texto = document.getElementById('texto')
+
+
+// function criptografar() {
+//     alert('texto')
+// }
+
+            
+// var resultado = document.getElementById('resultado')
+// resultado.innerText = `aaa`
+
+
+
+                        //3
+// function criptografar() {}
+// var msg = 'aaaaa'
+// switch (msg) {
+
+//     case a:
+//         msg = msg.replace(/a/gi,'b')
+//     // SEM break pra continuar rodando
+//     case b:
+//         msg = msg.replace(/b/gi,'c')
+    
+//     case c:
+//         msg = msg.replace(/c/gi,'d')
+    
+//     case d:
+//         msg = msg.replace(/d/gi,'e')
+    
+//     case e:
+//         msg = msg.replace(/e/gi,'f')
+    
+//     case f:
+//         msg = msg.replace(/f/gi,'g')
+    
+//     case g:
+//         msg = msg.replace(/g/gi,'h')
+    
+//     case h:
+//         msg = msg.replace(/h/gi,'i')
+    
+//     case i:
+//         msg = msg.replace(/i/gi,'j')
+
+//     case j:
+//         msg = msg.replace(/j/gi,'k')
+
+//     case k:
+//         msg = msg.replace(/k/gi,'l')
+
+//     case l:
+//         msg = msg.replace(/l/gi,'m')
+
+//     case m:
+//         msg = msg.replace(/m/gi,'n')
+
+//     case n:
+//         msg = msg.replace(/n/gi,'o')
+
+//     case o:
+//         msg = msg.replace(/o/gi,'p')
+
+//     case p:
+//         msg = msg.replace(/p/gi,'q')
+
+//     case q:
+//         msg = msg.replace(/q/gi,'r')
+
+//     case r:
+//         msg = msg.replace(/r/gi,'s')
+
+//     case s:
+//         msg = msg.replace(/s/gi,'t')
+
+//     case t:
+//         msg = msg.replace(/t/gi,'u')
+
+//     case u:
+//         msg = msg.replace(/u/gi,'v')
+
+//     case v:
+//         msg = msg.replace(/v/gi,'w')
+
+//     case w:
+//         msg = msg.replace(/w/gi,'x')
+
+//     case x:
+//         msg = msg.replace(/x/gi,'y')
+
+//     case y:
+//         msg = msg.replace(/y/gi,'z')
+
+//     case z:
+//         msg = msg.replace(/z/gi,'a')
+//   }
+
+// document.log(msg)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                                    //rascunho
+
+
+// > var frase = 'abcde'
+// undefined
+// > var veri = frase[2]
+// undefined
+// > console.log(veri)
+// i
+
+
+// var frase = 'eu nao aguento mais estudar'
+// var lugarDaLetra = frase[3]
+// var numero = frase.charCodeAt(3)
+// var frasee = 97
+
+// sentence = sentence.value.toUpperCase().replace(/n/g,'x')
+
+// console.log(lugarDaLetra)
+// console.log(numero)
+
+
+
+// var frasee = 97
+// console.log(String.fromCharCode(frasee))
+
+
+
+// var frase = "a a a a a a"
+
+// if (frase) {
+//     console.log(frase)
+// } else {
+//     console.log('deu errado')
+// }
+
+// console.log(frase.replace(/a a a/g, "z"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
